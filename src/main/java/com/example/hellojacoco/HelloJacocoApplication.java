4059daabc586318622c99f45adac32494910863e
ksm15 <ksm1538@naver.com>
2023-07-08 00:55:26 +0900

    init

---
+++ src/main/java/com/example/hellojacoco/HelloJacocoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellojacoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJacocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJacocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellojacoco/HelloJacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellojacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJacocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
